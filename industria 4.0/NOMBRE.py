0d7e82957b817009c42e95552abf866fc6e3b868
irenka-ferrusco <61525402+irenka-ferrusco@users.noreply.github.com>
2020-03-04 23:32:22 -0600

    Create NOMBRE.py

---
+++ industria 4.0/NOMBRE.py
@@ -0,0 +1,16 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> FISRT_NAME:"IRENKA"
+>>> LAST_NAME:"FERRUSCO SANTILLAN"
+>>> AGE="19"
+>>> HEIGHT:"1.70CM"
+>>> DRIVERS_LIC="FALSE"
+>>> WEIGHT:"69"
+>>> NUMBR:"771 131 5232"
+>>> PRINT("{:20}{};FORMAT("NAME:",FIRST_NAME,LAST_NAME))
+      
+SyntaxError: invalid syntax
+>>> PRINT("{:20}{};FORMAT("NAME:",FIRST_NAME,LAST_NAME))
+      
+SyntaxError: invalid syntax
+>>> 
