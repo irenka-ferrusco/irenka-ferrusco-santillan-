b7c55b231fdd895a56134f36139048abaac3ab19
irenka-ferrusco <61525402+irenka-ferrusco@users.noreply.github.com>
2020-03-11 18:50:02 -0600

    lo que falta
    programas de python

---
+++ python1.py
@@ -0,0 +1,11 @@
+"Data sheet"
+"string"
+print("name:{},{}".format(irenka, ferrusco))
+print("deseo:{}".format(ser alguien exitosa))
+"number"
+print("numero telefonico:{}".format(7711315232))
+print("edad:{}".format(19))
+print("crush:{}".format(yael, gea))
+"boolean"
+print("in love:{}".format(true))
+print("bateada:{}".format(true))
